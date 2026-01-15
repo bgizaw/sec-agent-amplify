cea0f0e8f6c0bcfff561463df395cd03517da006
Biruk Gizaw <bgizaw8@gmail.com>
2026-01-14 20:09:22 -0600

    testing template ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
-// import { useEffect, useState } from "react";
-// import type { Schema } from "../amplify/data/resource";
-// import { generateClient } from "aws-amplify/data";
-import { Header } from "./components/header";
-// import { Overview } from "@/components/overview";
-
-// const client = generateClient<Schema>();
+import "./App.css";
+import { Chat } from "./pages/chat";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-    <div className="flex flex-col min-w-0 h-dvh bg-background">
-      <Header />
-      <div
-        className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
-        // ref={messagesContainerRef}
-      >
-        {/* {messages.length == 0 && <Overview />}
-        {messages.map((message, index) => (
-          <PreviewMessage key={index} message={message} />
-        ))} */}
-        {/* {isLoading && <ThinkingMessage />}
-        <div
-          ref={messagesEndRef}
-          className="shrink-0 min-w-[24px] min-h-[24px]"
-        /> */}
-      </div>
-      {/* <div className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
-        <ChatInput
-          question={question}
-          setQuestion={setQuestion}
-          onSubmit={handleSubmit}
-          isLoading={isLoading}
-        />
-      </div> */}
-    </div>
+    <ThemeProvider>
+      <Router>
+        <div className="w-full h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+          <Routes>
+            <Route path="/" element={<Chat />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
