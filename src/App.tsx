fa221c9937e261a04c9862e86f5c4901e78e049c
Biruk Gizaw <bgizaw8@gmail.com>
2026-01-14 18:41:55 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useEffect, useState } from "react";
 // import type { Schema } from "../amplify/data/resource";
 // import { generateClient } from "aws-amplify/data";
-import { Header } from "@/components/header";
+import { Header } from "./components/header";
 // import { Overview } from "@/components/overview";
 
 // const client = generateClient<Schema>();
