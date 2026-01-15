7772b71f6fb4c77f67dd54c55d6a427240de7d8a
Biruk Gizaw <bgizaw8@gmail.com>
2026-01-14 18:56:44 -0600

    tracked components

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <>
+      <header className="flex items-center justify-between px-2 sm:px-4 py-2 bg-background text-black dark:text-white w-full">
+        <div className="flex items-center space-x-1 sm:space-x-2"></div>
+      </header>
+    </>
+  );
+};
